2b4d345149a8160337d8c86ce41d7abb635c0b08
johnedquinn <johnedquinn@gmail.com>
2021-03-30 17:11:24 -0400

    Add logs

--- cypress/integration/runner.js
+++ cypress/integration/runner.js
@@ -5,11 +5,16 @@ const TIMEOUT  = 3600000; // Timeout in milliseconds (class duration with buffer
 describe('Poll-Everywhere Test', () => {
   it('Logging in', () => {
 		cy.visit(POLL_URL)
+		cy.log('Visited', POLL_URL)
 
 		cy.get('input').type(USERNAME)
+		cy.log('Typed in username', USERNAME)
 
 		cy.contains('Continue').click()
+		cy.log('Clicking Continue')
 
 		cy.get('button.component-response-multiple-choice__option', { timeout: TIMEOUT }).should('not.be.disabled').first().click()
+		cy.log('Submitting First Option')
+		cy.log('Task Completed')
   })
 })
